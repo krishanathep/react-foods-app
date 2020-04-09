27d9b44125038334bec98a6fcbfd3ab134a85547
krishanathep <krishanathep@gmail.com>
2020-04-09 14:02:51 +0700

    Updates

---
+++ precache-manifest.f303064d41c295aa3fce55a197caf202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602b74d835a3d8314ae31c7aa54a0402",
+    "url": "/react-foods-app/index.html"
+  },
+  {
+    "revision": "f795135dffa2417ca21d",
+    "url": "/react-foods-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4de3e4f6752c9d864caa",
+    "url": "/react-foods-app/static/js/2.2bfff71d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-foods-app/static/js/2.2bfff71d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f795135dffa2417ca21d",
+    "url": "/react-foods-app/static/js/main.aa9dad55.chunk.js"
+  },
+  {
+    "revision": "0b1172e6759a9c344602",
+    "url": "/react-foods-app/static/js/runtime-main.07b142ab.js"
+  }
+]);
