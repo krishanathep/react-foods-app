2091b2e2a284b31a719da5b44c179ae176b0e1c1
krishanathep <39322839+krishanathep@users.noreply.github.com>
2020-04-10 17:08:20 +0700

    imput user component

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,10 +1,50 @@
 import React, { Component } from 'react'
 
 export class Users extends Component {
+    constructor() {
+        super()
+
+        this.state = {
+            users: [],
+            searchField: '',
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(users => this.setState({ users: users }))
+    }
+
     render() {
         return (
             <div className='Users'>
-                <h1 align='center'>This is and Users Component!</h1>   
+                <div className="row">
+                    <div className="col-md-12">
+                        <div className="card mt-5">
+                            <div className="card-body">
+                                <table className="table table-bordered">
+                                    <thead>
+                                        <tr>
+                                            <th>Image</th>
+                                            <th>Name</th>
+                                            <th>Email</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {this.state.users.map(user => (
+                                        <tr>
+                                                <td><img alt="picture" className='avatar' src={`https://robohash.org/${user.id}?set=set2&size=200x200`} /></td>
+                                            <td>{user.name}</td>
+                                            <td>{user.email}</td>
+                                        </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
